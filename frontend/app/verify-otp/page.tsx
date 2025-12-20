d21dea6610cdd04a3f5055c5a4cac28b088ebbab
mahilmithranks <mahilmithranks2007@gmail.com>
2025-12-20 15:09:26 +0530

    Remove OTP verification page

--- frontend/app/verify-otp/page.tsx
+++
@@ -1,9 +0,0 @@
-import { OTPVerification } from "@/components/ui/otp-verify";
-
-export default function OTPVerifyPage() {
-    return (
-        <div className="w-full">
-            <OTPVerification />
-        </div>
-    );
-}
